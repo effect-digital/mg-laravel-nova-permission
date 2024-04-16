30052955561b0c6aa71f692fde78cbdcb16274fb
Tom Woodward <164874562+TomEffect28@users.noreply.github.com>
2024-04-16 13:27:05 +0100

    MG-15821580 - Import request

--- src/LaravelNovaPermission.php
+++ src/LaravelNovaPermission.php
@@ -2,6 +2,7 @@
 
 namespace Insenseanalytics\LaravelNovaPermission;
 
+use Illuminate\Http\Request;
 use Laravel\Nova\Tool;
 use Laravel\Nova\Nova;
 
@@ -81,7 +82,7 @@ class LaravelNovaPermission extends Tool
     /**
      * Build the menu that renders the navigation links for the tool.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @return mixed
      */
     public function menu(Request $request): mixed
